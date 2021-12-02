55b7ea4abfb0fdb0584aa63a175fecf6017173f4
Thalixte <thalixte@sfr.fr>
2021-12-02 22:19:19 +0100

    add outputtoseparateconsole option

--- utils/msfs_sdk.py
+++ utils/msfs_sdk.py
@@ -6,6 +6,7 @@ from utils import ScriptError
 
 MSFS_BUILD_EXE_FORCE_STEAM_OPTION = "-forcesteam"
 MSFS_BUILD_EXE_OUTPUT_DIR_OPTION = "-outputdir"
+MSFS_BUILD_EXE_OUTPUT_TO_SEPARATE_CONSOLE_OPTION = "-outputtoseparateconsole"
 ERROR_MSG = "MSFS SDK tools not installed"
 
 
@@ -13,13 +14,13 @@ ERROR_MSG = "MSFS SDK tools not installed"
 # build scenery into new MSFS package
 ######################################################
 def build_package(msfs_project, settings):
-    fspackagetool_exe_path = os.path.join(settings.fspackagetool_folder, MSFS_BUILD_EXE)
+    fspackagetool_exe_path = "\"" + os.path.join(settings.fspackagetool_folder, MSFS_BUILD_EXE) + "\""
 
     if settings.msfs_steam_version:
         fspackagetool_exe_path += " " + MSFS_BUILD_EXE_FORCE_STEAM_OPTION
 
     try:
-        print(fspackagetool_exe_path + " \"" + msfs_project.project_definition_xml_path + "\" " + MSFS_BUILD_EXE_OUTPUT_DIR_OPTION + " \"" + msfs_project.project_folder + "\"")
-        subprocess.run(fspackagetool_exe_path + " \"" + msfs_project.project_definition_xml_path + "\" " + MSFS_BUILD_EXE_OUTPUT_DIR_OPTION + " \"" + msfs_project.project_folder + "\"", shell=False, check=True)
+        print(fspackagetool_exe_path + " " + MSFS_BUILD_EXE_OUTPUT_TO_SEPARATE_CONSOLE_OPTION + " \"" + msfs_project.project_definition_xml_path + "\" " + MSFS_BUILD_EXE_OUTPUT_DIR_OPTION + " \"" + msfs_project.project_folder + "\"")
+        subprocess.run(fspackagetool_exe_path + " " + MSFS_BUILD_EXE_OUTPUT_TO_SEPARATE_CONSOLE_OPTION + " \"" + msfs_project.project_definition_xml_path + "\" " + MSFS_BUILD_EXE_OUTPUT_DIR_OPTION + " \"" + msfs_project.project_folder + "\"", shell=True, check=False)
     except:
         raise ScriptError(ERROR_MSG)
