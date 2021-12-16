2337595059e8211a7c2bcb10bdcd7b8f8bf1523e
Thalixte <thalixte@sfr.fr>
2021-12-16 22:11:31 +0100

    test menu operator

--- menu.py
+++ menu.py
@@ -17,8 +17,53 @@
 # ##### END GPL LICENSE BLOCK #####
 
 # <pep8 compliant>
-import os, bpy
+import os
+import argparse
+import site
+import sys
+import bpy
+
 from bpy.types import Header, Menu, Panel
+
+CONSTANTS_FOLDER = "constants"
+UTILS_FOLDER = "utils"
+SCRIPT_FOLDER = "scripts"
+
+# Check if script is executed in Blender and get absolute path of current folder
+if bpy.context.space_data is not None:
+    files_dir = os.path.dirname(bpy.context.space_data.text.filepath)
+else:
+    files_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Get scripts folder and add it to the search path for modules
+os.chdir(files_dir)
+
+sys.path.append(site.USER_SITE)
+
+if files_dir not in sys.path:
+    sys.path.append(files_dir)
+
+# Get scripts folder and add it to the search path for modules
+cwd = os.path.join(files_dir, CONSTANTS_FOLDER)
+if cwd not in sys.path:
+    sys.path.append(cwd)
+
+# Get scripts folder and add it to the search path for modules
+cwd = os.path.join(files_dir, UTILS_FOLDER)
+if cwd not in sys.path:
+    sys.path.append(cwd)
+
+# Get scripts folder and add it to the search path for modules
+cwd = os.path.join(files_dir, SCRIPT_FOLDER)
+if cwd not in sys.path:
+    sys.path.append(cwd)
+
+from utils import *
+from blender import clean_scene
+from msfs_project import MsfsLod
+
+
+settings = Settings(get_sources_path())
 
 
 class TOPBAR_MT_google_earth_optimization_menus(Menu):
@@ -36,12 +81,32 @@ class TOPBAR_MT_google_earth_optimization_menu(Menu):
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("wm.splash")
+        layout.operator("screen.init_msfs_scenery_project_operator")
+
+
+class InitMsfsSceneryProjectOperator(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "screen.init_msfs_scenery_project_operator"
+    bl_label = "Init a new MSFS scenery project"
+
+    @classmethod
+    def poll(cls, context):
+        return not os.path.isdir(os.path.join(settings.projects_path, settings.project_name))
+
+    def execute(self, context):
+        script_file = "init_msfs_scenery_project_script.py"
+        text = bpy.data.texts.load(os.path.join(cwd, script_file))   # if exists in blend
+        ctx = bpy.context.copy()
+        ctx[script_file] = text
+        lib1 = bpy.data.texts[script_file].as_string()
+        exec(text)
+        return {'FINISHED'}
 
 
 classes = (
     TOPBAR_MT_google_earth_optimization_menus,
     TOPBAR_MT_google_earth_optimization_menu,
+    InitMsfsSceneryProjectOperator
 )
 
 
