984ddecc6f8cc74495022fe5723c497ee7c9d57d
Thalixte <thalixte@sfr.fr>
2021-12-21 19:14:57 +0100

    add optimize msfs scenery panel

--- UI/menu.py
+++ UI/menu.py
@@ -2,12 +2,12 @@
 import os
 
 import bpy
-from bpy.props import IntProperty, BoolProperty
+from bpy.props import IntProperty
 from bpy.types import Menu
 from bpy_extras.io_utils import ImportHelper
 from bpy_types import Operator
 from constants import CLEAR_CONSOLE_CMD
-from utils import exec_script_from_menu, Settings, get_sources_path, isolated_print
+from utils import exec_script_from_menu, Settings, get_sources_path
 
 settings = Settings(get_sources_path())
 settings.save()
@@ -21,15 +21,15 @@ def reload_topbar_menu():
 
 
 def projects_path_updated(self, context):
-    settings.projects_path = context.scene.custom_props.projects_path
+    settings.projects_path = context.scene.settings.projects_path
     settings.save()
     panel_props = context.scene.panel_props
     context.window.cursor_warp(panel_props.first_mouse_x, panel_props.first_mouse_y)
-    bpy.ops.wm.init_msfs_scenery_project('INVOKE_DEFAULT')
+    bpy.ops.wm.init_msfs_scenery_project_panel('INVOKE_DEFAULT')
 
 
 def project_name_updated(self, context):
-    settings.project_name = context.scene.custom_props.project_name
+    settings.project_name = context.scene.settings.project_name
     settings.save()
 
 
@@ -48,10 +48,10 @@ class TOPBAR_MT_google_earth_optimization_menu(Menu):
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("wm.init_msfs_scenery_project")
+        layout.operator("wm.init_msfs_scenery_project_panel")
 
 
-class CustomPropertyGroup(bpy.types.PropertyGroup):
+class SettingsPropertyGroup(bpy.types.PropertyGroup):
     # float_slider: bpy.props.FloatProperty(name='float value', soft_min=0, soft_max=10)
     # int_slider: bpy.props.IntProperty(name='int value', soft_min=0, soft_max=10)
     # bool_toggle: bpy.props.BoolProperty(name='bool toggle')
@@ -77,17 +77,48 @@ class PanelPropertyGroup(bpy.types.PropertyGroup):
     first_mouse_y: IntProperty(default=0)
 
 
-class VIEW_3D_PT_CustomToolShelf(Operator):
-    bl_idname = 'wm.init_msfs_scenery_project'
+class OT_InitMsfsSceneryPanel(Operator):
+    bl_idname = 'wm.init_msfs_scenery_project_panel'
     bl_label = 'Initialize a new MSFS project scenery'
     bl_options = {'REGISTER', 'UNDO'}
 
     def draw(self, context):
         layout = self.layout
         subrow = layout.row(align=True)
-        layout.prop(context.scene.custom_props, 'projects_path')
+        layout.prop(context.scene.settings, 'projects_path')
         layout.separator()
-        layout.prop(context.scene.custom_props, 'project_name')
+        layout.prop(context.scene.settings, 'project_name')
+
+    def invoke(self, context, event):
+        panel_props = context.scene.panel_props
+        if panel_props.first_mouse_x == 0 and panel_props.first_mouse_y == 0:
+            panel_props.first_mouse_x = event.mouse_x
+            panel_props.first_mouse_y = event.mouse_y
+        # context.window.cursor_warp(context.window.width / 2, (context.window.height / 2) + 60)
+        context.window_manager.invoke_props_dialog(self, width=600)
+        return {'RUNNING_MODAL'}
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+    def execute(self, context):
+        script_file = "init_msfs_scenery_project.py"
+        exec_script_from_menu(os.path.join(settings.sources_path, script_file))
+        return {'FINISHED'}
+
+
+class OT_OptimizeSceneryPanel(Operator):
+    bl_idname = 'wm.optimise_scenery_panel'
+    bl_label = 'Optimize a MSFS scenery'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def draw(self, context):
+        layout = self.layout
+        subrow = layout.row(align=True)
+        layout.prop(context.scene.settings, 'projects_path')
+        layout.separator()
+        layout.prop(context.scene.settings, 'project_name')
 
     def invoke(self, context, event):
         panel_props = context.scene.panel_props
@@ -128,16 +159,6 @@ class ProjectsPathOperator(Operator, ImportHelper):
         pass
 
 
-class InitMsfsSceneryProjectOperator(Operator):
-    bl_idname = 'text.init_msfs_scenery_project'
-    bl_label = 'Initialize a new MSFS project scenery...'
-    bl_options = {'REGISTER', 'UNDO'}
-
-    def execute(self, context):
-        script_file = "init_msfs_scenery_project.py"
-        exec_script_from_menu(os.path.join(settings.sources_path, script_file))
-        return {'FINISHED'}
-
 bl_info = {
     "name": "Ui test addon",
     "category": "tests"
@@ -146,10 +167,10 @@ bl_info = {
 classes = (
     TOPBAR_MT_google_earth_optimization_menus,
     TOPBAR_MT_google_earth_optimization_menu,
-    CustomPropertyGroup,
+    SettingsPropertyGroup,
     PanelPropertyGroup,
-    InitMsfsSceneryProjectOperator,
-    VIEW_3D_PT_CustomToolShelf,
+    OT_InitMsfsSceneryPanel,
+    OT_OptimizeSceneryPanel,
 )
 
 
@@ -160,7 +181,7 @@ def register():
         except ValueError:
             pass
 
-    bpy.types.Scene.custom_props = bpy.props.PointerProperty(type=CustomPropertyGroup)
+    bpy.types.Scene.settings = bpy.props.PointerProperty(type=SettingsPropertyGroup)
     bpy.types.Scene.panel_props = bpy.props.PointerProperty(type=PanelPropertyGroup)
     bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_google_earth_optimization_menus.draw)
 
@@ -168,7 +189,7 @@ def register():
 def unregister():
     bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_google_earth_optimization_menus.draw)
 
-    del bpy.types.Scene.custom_props
+    del bpy.types.Scene.settings
     del bpy.types.Scene.panel_props
     for cls in classes:
         bpy.utils.unregister_class(cls)
