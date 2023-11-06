9cdfd855741c4349254eabc98c51ffa6d692bce7
thalixte <thalixte@sfr.fr>
2023-11-06 18:44:08 +0100

    fix typo

--- msfs_project/project.py
+++ msfs_project/project.py
@@ -1886,8 +1886,8 @@ class MsfsProject:
                 input_fd, output_fd = os.pipe()
 
                 for obj in chunck:
-                    isolated_print("-------------------------------------------------------------------------------")
-                    isolated_print("\"" + str(bpy.app.binary_path) + "\" --background --python \"" + os.path.join(os.path.dirname(os.path.dirname(__file__)), script_name) + "\" -- " + str(" ").join(obj["params"]))
+                    print("-------------------------------------------------------------------------------")
+                    print("\"" + str(bpy.app.binary_path) + "\" --background --python \"" + os.path.join(os.path.dirname(os.path.dirname(__file__)), script_name) + "\" -- " + str(" ").join(obj["params"]))
 
                 si = subprocess.STARTUPINFO()
                 si.dwFlags = subprocess.STARTF_USESTDHANDLES | subprocess.HIGH_PRIORITY_CLASS
