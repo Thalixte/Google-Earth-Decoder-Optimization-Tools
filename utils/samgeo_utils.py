a7c287cea165bb9f7afb2d3e85dc2b584dde03a6
thalixte <thalixte@sfr.fr>
2023-12-30 18:31:02 +0100

    convert tif to png

--- utils/samgeo_utils.py
+++ utils/samgeo_utils.py
@@ -24,6 +24,7 @@ import numpy as np
 from constants import TORCH_LIB, SAMGEO_LIB
 from utils import isolated_print
 from utils.install_lib import install_python_lib
+from PIL import Image, ImageSequence
 
 try:
     import torch
@@ -44,7 +45,7 @@ import samgeo
 
 def samgeo_sandbox(output_path, bbox=None, source_image=None):
     lbbox= [bbox[2], bbox[1], bbox[3], bbox[0]]
-    zoom = 18
+    zoom = 17
     isolated_print(get_basemaps().keys())
 
     sam = SamGeo(
@@ -55,7 +56,7 @@ def samgeo_sandbox(output_path, bbox=None, source_image=None):
     lbboxes = createZoneTable(2, westlimit=lbbox[0], southlimit=lbbox[1], eastlimit=lbbox[2], northlimit=lbbox[3])
 
     tiles_dir = os.path.join(output_path, "tiles")
-    masks_dir = os.path.join(output_path, "masks")
+    # masks_dir = os.path.join(output_path, "masks")
 
     try:
         shutil.rmtree(tiles_dir)
@@ -71,55 +72,65 @@ def samgeo_sandbox(output_path, bbox=None, source_image=None):
     if source_image is None:
         image = 'satellite.tif'
         merge_rasters(tiles_dir, os.path.join(output_path, image))
-    else:
-        image = source_image
 
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    isolated_print(device, "selected")
-
-    mask = "segments.tif"
-    sam.generate(
-        os.path.join(output_path, image),
-        os.path.join(output_path, mask),
-        batch=True,
-        foreground=True,
-        erosion_kernel=(3, 3),
-        mask_multiplier=255,
-        device=device
-    )
-
-    # Ensure to remove remaining cache folder
-    try:
-        shutil.rmtree(tiles_dir)
-        shutil.rmtree(masks_dir)
-    except:
-        pass
-
-    os.makedirs(masks_dir, exist_ok=True)
-
-    # split_raster(os.path.join(output_path, image), out_dir=tiles_dir, tile_size=(1000, 1000), overlap=0)
-
-    sam = LangSAM()
-
-    text_prompt = "tree"
-
-    # sam.predict_batch(
-    #     images=tiles_dir,
-    #     out_dir=masks_dir,
-    #     text_prompt=text_prompt,
-    #     box_threshold=0.26,
-    #     text_threshold=0.26,
+    im = Image.open(os.path.join(output_path, image))
+    for i, page in enumerate(ImageSequence.Iterator(im)):
+        output_png_path = os.path.join(output_path, image.replace(".tif", ".png"))
+        isolated_print(output_png_path)
+        if not os.path.isfile(output_png_path):
+            try:
+                page.save(output_png_path)
+            except:
+                pass
+    # else:
+    #     image = source_image
+    #
+    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # isolated_print(device, "selected")
+    #
+    # mask = "segments.tif"
+    # sam.generate(
+    #     os.path.join(output_path, image),
+    #     os.path.join(output_path, mask),
+    #     batch=True,
+    #     foreground=True,
+    #     erosion_kernel=(3, 3),
     #     mask_multiplier=255,
-    #     device=device,
-    #     dtype='uint8',
-    #     merge=True,
-    #     verbose=True,
+    #     device=device
     # )
-
-    # merge_rasters(masks_dir, os.path.join(output_path, "segment.tif"))
-
-    shapefile = "segment.shp"
-    sam.raster_to_vector(os.path.join(output_path, "segment.tif"), os.path.join(output_path, shapefile))
+    #
+    # # Ensure to remove remaining cache folder
+    # try:
+    #     shutil.rmtree(tiles_dir)
+    #     shutil.rmtree(masks_dir)
+    # except:
+    #     pass
+    #
+    # os.makedirs(masks_dir, exist_ok=True)
+    #
+    # # split_raster(os.path.join(output_path, image), out_dir=tiles_dir, tile_size=(1000, 1000), overlap=0)
+    #
+    # sam = LangSAM()
+    #
+    # text_prompt = "tree"
+    #
+    # # sam.predict_batch(
+    # #     images=tiles_dir,
+    # #     out_dir=masks_dir,
+    # #     text_prompt=text_prompt,
+    # #     box_threshold=0.26,
+    # #     text_threshold=0.26,
+    # #     mask_multiplier=255,
+    # #     device=device,
+    # #     dtype='uint8',
+    # #     merge=True,
+    # #     verbose=True,
+    # # )
+    #
+    # # merge_rasters(masks_dir, os.path.join(output_path, "segment.tif"))
+    #
+    # shapefile = "segment.shp"
+    # sam.raster_to_vector(os.path.join(output_path, "segment.tif"), os.path.join(output_path, shapefile))
 
 
 def createZoneTable(zone_factor, westlimit, southlimit, eastlimit, northlimit):
